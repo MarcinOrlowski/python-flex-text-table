745c8a1f852f603e7e8daf707516aeb93cba4fde
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2024-09-26 08:57:59 +0200

    Updated source headers

--- flextable/align.py
+++ flextable/align.py
@@ -3,7 +3,7 @@
 # Flex Text Table
 # Fast and flexible Pyhon library for text tables.
 #
-# Copyright ©2023 Marcin Orlowski <mail [@] MarcinOrlowski.com>
+# Copyright ©2023-2024 Marcin Orlowski <mail [@] MarcinOrlowski.com>
 # https://github.com/MarcinOrlowski/python-flex-text-table/
 #
 ####################################################################################################
